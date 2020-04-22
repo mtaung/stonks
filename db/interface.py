02c166bfe9d84bf13f1fd8e01bcb8a1e70cdb7ea
SchrodingersKitty <radium213@gmail.com>
2020-04-22 23:48:35 +0300

    updated db interface

--- db/interface.py
+++ db/interface.py
@@ -1,51 +1,31 @@
 from sqlalchemy import create_engine, literal
 from sqlalchemy.orm import sessionmaker
-from . import tables 
-
-class BasicInterface:
-
-    def __init__(self, table, session):
-        self.Table = table 
-        self.session = session
-
-    def get(self, id):
-        return self.session.query(self.Table).filter(self.Table.id == id).first()
-
-    def add(self, **kwargs):
-        if kwargs:
-            dbo = self.Table(**kwargs)
-            self.session.add(dbo)
-            self.session.flush()
-            return dbo
-        return None
-
-    def count(self):
-        return self.session.query(self.Table).count()
-
-    def getrow(self, row):
-        return self.session.query(self.Table).offset(row).first()
-
-class StockInterface(BasicInterface):
-
-    def company_inventory(self, company):
-        return self.session.query(self.Table).filter(self.Table.company == company)
-
-class CompanyInterface(BasicInterface):
-
-    def get_active(self, id):
-        return self.session.query(self.Table).filter(self.Table.owner == id).filter(self.Table.active).first()
 
 class DatabaseInterface:
-
     def __init__(self, url):
         engine = create_engine(url)
         Session = sessionmaker(bind=engine)
-        self.main_session = Session()
-        self.users = BasicInterface(tables.User, self.main_session)
-        self.history = BasicInterface(tables.History, self.main_session)
-        self.companies = CompanyInterface(tables.Company, self.main_session)
-        self.symbols = BasicInterface(tables.Symbol, self.main_session)
-        self.closes = BasicInterface(tables.Close, self.main_session)
+        self.session = Session()
+    
+    def getall(self, Table, **kwargs):
+        q = self.session.query(Table)
+        for key in kwargs:
+            q = q.filter(getattr(Table, key) == kwargs[key])
+        return q
+    
+    def get(self, Table, **kwargs):
+        q = self.getall(Table, **kwargs)
+        return q.first()
+    
+    def add(self, obj):
+        self.session.add(obj)
+        return None
+    
+    def count(self, Table):
+        return self.session.query(Table).count()
+    
+    def delete(self, obj):
+        self.session.delete(obj)
     
     def commit(self):
-        self.main_session.commit()
+        self.session.commit()
--- modules/iex.py
+++ modules/iex.py
@@ -4,11 +4,11 @@ from datetime import datetime
 from datetime import timedelta
 from utils import config
 from db.interface import DatabaseInterface
+from db.tables import Symbol, Close
 
 class Iex:
 
     def __init__(self, token):
-        
         self.token = token
         self.db = DatabaseInterface('sqlite:///stonks.db')
 
@@ -17,18 +17,25 @@ class Iex:
         quote = stonk.get_quote()
         return quote['latestPrice']
 
-    def symbols(self):
+    def update_symbols(self):
         symbols = get_symbols(token = self.token)
-
+        # handle adding of symbols
         for symbol in symbols:
-            
-            if symbol['symbol']
-
-        return symbols
+            sym = symbol['symbol']
+            if not self.db.get(Symbol, symbol=sym):
+                self.db.add(Symbol(symbol=sym, name=symbol['name'], stock_type=symbol['type']))
+        self.db.commit()
+        # handle removal of symbols?
+        # I think this is a bit more complicated, because what happens if someone owns stock etc
+        # leave for later
+    
+    def symbols(self):
+        return self.db.getall(Symbol)
     
     def history(self, symbol):
-        start = datetime.now() - timedelta(days=3)
-        close_history = stocks.get_historical_data(symbol, start=start, end=datetime.now(), close_only=True, output_format='json', token=self.token)
-        return close_history
-
-    
+        delta = timedelta(days=3)
+        end = datetime.now()
+        start = end - delta
+        cached_history = self.db.getall(Close, symbol=symbol)
+        #close_history = stocks.get_historical_data(symbol, start=start, end=datetime.now(), close_only=True, output_format='json', token=self.token)
+        #return close_history
