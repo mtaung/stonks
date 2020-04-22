27cb89e6d940a05a7da702845f0c82d3424b9f93
SchrodingersKitty <radium213@gmail.com>
2020-04-22 23:48:35 +0300

    updated db interface

--- modules/stonks.py
+++ modules/stonks.py
@@ -1,6 +1,7 @@
 import discord, datetime
 from discord.ext import commands
 from db.interface import DatabaseInterface
+from db.tables import User, Company, History
 
 class Stonks(commands.Cog):
     def __init__(self, bot):
@@ -14,18 +15,18 @@ class Stonks(commands.Cog):
         uid = author.id
         uname = author.nick if hasattr(author, 'nick') else author.name
         
-        if not self.db.users.get(uid):
-            self.db.users.add(id=uid, credit_score=0)
+        if not self.db.get(User, id=uid):
+            self.db.add(User(id=uid, credit_score=0))
             await ctx.send(f'Welcome to the stonks market, {uname}. We have added you to our registry.')
         
-        active_company = self.db.companies.get_active(uid)
+        active_company = self.db.get(Company, owner=uid, active=True)
         if active_company:
             await ctx.send(f'You are already in ownership of the registered company {active_company.name}, {uname}.')
 
         else:
-            company = self.db.companies.add(owner=uid, name=company_name, balance=10000, active=True)
-            print(company.id, company.name)
-            self.db.history.add(company=company.id, date=datetime.datetime.now())
+            company = Company(owner=uid, name=company_name, balance=10000, active=True)
+            self.db.add(company)
+            self.db.add(History(company=company.id, date=datetime.datetime.now()))
             self.db.commit()
             await ctx.send(f'Your application to register {company_name} has been accepted. Happy trading!')
 
