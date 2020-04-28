fe145e14f87ef26d0148c27d6a7471665b8d04e1
SchrodingersKitty <radium213@gmail.com>
2020-04-28 23:16:31 +0300

    switch to scoped_session

--- db/interface.py
+++ db/interface.py
@@ -2,17 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 engine = create_engine('sqlite:///stonks.db')
-Session = sessionmaker(bind=engine)
+Session = scoped_session(sessionmaker(bind=engine))
 
 class DB:
     def __enter__(self):
-        self.sess = Session()
-        return self.sess
+        return Session()
     def __exit__(self, type, value, traceback):
         if not value:
             # commit only if no exception occured
-            self.sess.commit()
-        self.sess.close()
+            Session.commit()
+        #self.sess.close()
+        Session.remove()
 
 def _list(list_of_tuples):
     return [v for (v,) in list_of_tuples]
